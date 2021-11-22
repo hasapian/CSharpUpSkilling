52fd8dc41f6427a9e9ed6013cfd32903d0e3d623
s.chasakos <s.chasakos@kaizengaming.com>
2021-11-22 11:18:32 +0200

    Exercise 3

---
+++ Lesson4/Exercise3/MyClass.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Exercise3
+{
+    class MyClass
+    {
+        public static bool MyMethod(string myString, out double myDouble)
+        {
+            if (double.TryParse(myString, out myDouble) && (myDouble <0))         
+                return true;
+            else
+                return false;
+        }
+    }
+}
---
+++ Lesson4/Exercise3/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Exercise3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {            
+            int i = 0;
+            double numericValue;
+            do
+            {
+                Console.WriteLine("Enter string");
+                string stringEntered = Console.ReadLine();
+                var result = MyClass.MyMethod(stringEntered, out numericValue);
+                if (result)
+                    Console.WriteLine("Numeric value: {0}", numericValue);
+                i++;
+            } while (i < 3);
+        }
+    }
+}
